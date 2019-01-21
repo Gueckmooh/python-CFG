eaa9cd444dd8ce60c2875a0c7be073cab2ecfd6c
Enzo Brignon <gueckmooh@gmail.com>
2019-01-21 19:45:07 +0100

    ajout example switch case

---
+++ tests/example7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int
+main (void)
+{
+  int i = 0;
+  switch (i)
+  {
+    case 0:
+      printf ("0\n");
+    case 1:
+      printf ("1\n");
+    case 2:
+      printf ("2\n");
+    default:
+      printf ("auther\n");
+  }
+}
+
